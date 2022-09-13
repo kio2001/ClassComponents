d75580035febc2fc67938c477cb4ef6ca61000e8
kio2001 <112913150+kio2001@users.noreply.github.com>
2022-09-13 10:24:46 +0900

    src 追加

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calc from './pages/Calc';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Calc />
+      </header>
+    </div>
+  );
+}
+
+export default App;
